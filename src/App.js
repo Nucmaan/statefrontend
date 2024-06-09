b63ad1170480e7f51c9bc68250986beea2e85cff
MOHAMED ABDIHAKIN MUHUMED <48615938+Nucmaan@users.noreply.github.com>
2024-06-09 22:27:54 +0800

    router Pages
    router Pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./Auth/Login";
+import Navbar from "./Components/Navbar";
+import Footer from "./Components/Footer";
+import HomaPage from "./Components/HomaPage";
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline bg-black text-white">
-      Hello world!
-    </h1>
-  )
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomaPage />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
 }
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -2,7 +2,9 @@ import React from 'react'
 
 function Navbar() {
   return (
-    <div>Navbar</div>
+    <div>
+    <h1>navbar </h1>
+    </div>
   )
 }
 
