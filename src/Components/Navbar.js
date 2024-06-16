66ff4cc1f402d43742e260568e9c82a9bd44ac2e
MOHAMED ABDIHAKIN MUHUMED <48615938+Nucmaan@users.noreply.github.com>
2024-06-16 23:48:10 +0800

    complete responsive
    complete responsive

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,9 +1,14 @@
 import React, { useState } from "react";
 import { AiOutlineClose } from "react-icons/ai";
+import { GiHamburgerMenu } from "react-icons/gi";
 
 function Navbar() {
   const [navbar, setNavbar] = useState(false);
-  
+
+  const handlenav = () => {
+    setNavbar(!navbar);
+  };
+
   return (
     <div className="h-16  shadow-md mx-auto px-4 text-black">
       <div className="flex justify-between items-center">
@@ -13,35 +18,48 @@ function Navbar() {
 
         <div>
           <ul className="flex hidden md:flex">
-            <li className="p-4 cursor-pointer">Home</li>
-            <li className="p-4 cursor-pointer">Property</li>
-            <li className="p-4 cursor-pointer">About Us</li>
-            <li className="p-4 cursor-pointer">Contact Us</li>
+            <li className="p-4 cursor-pointer uppercase">Home</li>
+            <li className="p-4 cursor-pointer  uppercase">Property</li>
+            <li className="p-4 cursor-pointer uppercase">About Us</li>
+            <li className="p-4 cursor-pointer uppercase">Contact Us</li>
           </ul>
         </div>
 
         <div>
           <ul className="hidden flex items-center text-center  md:flex">
-            <li className="p-4 cursor-pointer">Login</li>
-            <li className="p-4 cursor-pointer">Register</li>
+            <li className="p-4 cursor-pointer uppercase">Login</li>
+            <li className="p-4 cursor-pointer uppercase">Register</li>
           </ul>
         </div>
 
-        <div className="md:hidden flex items-center pt-2">
-          <AiOutlineClose size={30} />
+        <div className="md:hidden flex items-center pt-2" onClick={handlenav}>
+          {!navbar ? (
+            <AiOutlineClose size={30} />
+          ) : (
+            <GiHamburgerMenu size={30} />
+          )}
         </div>
       </div>
 
-      <ul className="w-full absolute flex flex-col  justify-center items-center left-0 gap-y-2 pt-5  md:hidden">
-        <li className="w-full flex justify-center p-4 border-b border-gray-600 cursor-pointer">Home</li>
-        <li className="w-full flex justify-center border-b border-gray-600 p-4 cursor-pointer">Property</li>
-        <li className="w-full flex justify-center border-b border-gray-600  p-4 cursor-pointer">About Us</li>
-        <li className="w-full flex justify-center border-b border-gray-600  p-4 cursor-pointer">Contact Us</li>
+      <ul className={!navbar ? 'w-full absolute flex flex-col  justify-center items-center left-0 gap-y-2 pt-5  md:hidden' : 'hidden'}>
+
+        <li className="w-full flex justify-center p-4 border-b border-gray-600 cursor-pointer uppercase">
+          Home
+        </li>
+        <li className="w-full flex justify-center border-b border-gray-600 p-4 cursor-pointer uppercase">
+          Property
+        </li>
+        <li className="w-full flex justify-center border-b border-gray-600  p-4 cursor-pointer uppercase">
+          About Us
+        </li>
+        <li className="w-full flex justify-center border-b border-gray-600  p-4 cursor-pointer uppercase">
+          Contact Us
+        </li>
         <div className="w-full flex flex-col ">
-          <button className="mb-4 bg-blue-600 text-white p-2 font-bold ">
+          <button className="mb-4 bg-blue-600 text-white p-2 font-bold uppercase">
             Login
           </button>
-          <button className="mb-4 bg-blue-600 text-white p-2 font-bold">
+          <button className="mb-4 bg-blue-600 text-white p-2 font-bold uppercase">
             Register
           </button>
         </div>
