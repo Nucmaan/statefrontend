e20ba4ad666a572efa17c06e3300e358b16e2544
MOHAMED ABDIHAKIN MUHUMED <48615938+Nucmaan@users.noreply.github.com>
2024-06-27 16:03:31 +0800

    Adding All Routs
    Adding All Routs

--- src/App.js
+++ src/App.js
@@ -2,16 +2,25 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./Auth/Login";
 import Navbar from "./Components/Navbar";
 import Footer from "./Components/Footer";
-import HomaPage from "./Components/HomaPage";
+import Register from "./Auth/Register";
+import AboutUs from "./Pages/AboutUs";
+import ContactUs from "./Pages/ContactUs";
+import PropertyList from "./Pages/PropertyList";
+import Herro from "./Components/Herro";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Navbar />
       <Routes>
-       
+        <Route path="/" element={<Herro />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/About" element={<AboutUs />} />
+        <Route path="/Contact" element={<ContactUs />} />
+        <Route path="/Property" element={<PropertyList />} />
       </Routes>
-      
+      <Footer />
     </BrowserRouter>
   );
 }
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 import React, { useState } from "react";
 import { AiOutlineClose } from "react-icons/ai";
 import { GiHamburgerMenu } from "react-icons/gi";
@@ -13,22 +15,22 @@ function Navbar() {
     <div className="  shadow-md mx-auto px-4 text-black">
       <div className="flex justify-between items-center">
         <div>
-          <h1 className="w-full text-3xl font-bold ">Nucmaan</h1>
+          <h1 className="w-full text-3xl font-bold "><Link to="/">Nucmaan</Link></h1>
         </div>
 
         <div>
           <ul className="flex hidden md:flex">
-            <li className="p-4 cursor-pointer uppercase">Home</li>
-            <li className="p-4 cursor-pointer  uppercase">Property</li>
-            <li className="p-4 cursor-pointer uppercase">About Us</li>
-            <li className="p-4 cursor-pointer uppercase">Contact Us</li>
+            <li className="p-4 cursor-pointer uppercase"><Link to="/">Home</Link> </li>
+            <li className="p-4 cursor-pointer  uppercase"><Link to="Property">Property</Link></li>
+            <li className="p-4 cursor-pointer uppercase"><Link to="About">About Us</Link></li>
+            <li className="p-4 cursor-pointer uppercase"><Link to="Contact">Contact Us</Link></li>
           </ul>
         </div>
 
         <div>
           <ul className="hidden flex items-center text-center  md:flex">
-            <li className="p-4 cursor-pointer uppercase">Login</li>
-            <li className="p-4 cursor-pointer uppercase">Register</li>
+            <li className="p-4 cursor-pointer uppercase"><Link to="login">Login</Link></li>
+            <li className="p-4 cursor-pointer uppercase"><Link to="register">Register</Link></li>
           </ul>
         </div>
 
@@ -42,25 +44,22 @@ function Navbar() {
       </div>
 
       <ul className={!navbar ? 'w-full absolute flex flex-col  justify-center items-center left-0 gap-y-2 pt-5  md:hidden' : 'hidden'}>
-
-        <li className="w-full flex justify-center p-4 border-b border-gray-600 cursor-pointer uppercase">
-          Home
-        </li>
+    
         <li className="w-full flex justify-center border-b border-gray-600 p-4 cursor-pointer uppercase">
-          Property
+        <Link to="Property">Property</Link>
         </li>
         <li className="w-full flex justify-center border-b border-gray-600  p-4 cursor-pointer uppercase">
-          About Us
+        <Link to="About">About Us</Link>
         </li>
         <li className="w-full flex justify-center border-b border-gray-600  p-4 cursor-pointer uppercase">
-          Contact Us
+        <Link to="Contact">Contact Us</Link>
         </li>
         <div className="w-full flex flex-col ">
           <button className="mb-4 bg-blue-600 text-white p-2 font-bold uppercase">
-            Login
+          <Link to="login">Login</Link>
           </button>
           <button className="mb-4 bg-blue-600 text-white p-2 font-bold uppercase">
-            Register
+          <Link to="register">Register</Link>
           </button>
         </div>
       </ul>
