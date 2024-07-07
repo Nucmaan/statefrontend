d5d4a8ab98946d3f773083e156e3ccb3e8aef41a
MOHAMED ABDIHAKIN MUHUMED <48615938+Nucmaan@users.noreply.github.com>
2024-07-08 00:50:45 +0800

    logo favcon
    logo favcon

--- src/Auth/Login.js
+++ src/Auth/Login.js
@@ -1,9 +1,45 @@
-import React from 'react'
+import { Link } from "react-router-dom";
+import React from "react";
 
 function Login() {
   return (
-    <div>Login</div>
-  )
+    <div className="mx-5">
+
+    <div className="flex flex-col justify-center mx-10 shadow-lg mt-10 p-5 rounded-md">
+    
+    <h1 className=" font-bold text-3xl text-center">Login</h1>
+
+      <form>
+        <div className="mt-6">
+          <input
+            type="email"
+            placeholder="Email"
+            className="border-2 border-blue-600 p-2 w-full rounded-md"
+          />
+        </div>
+
+        <div className="mt-6">
+          <input
+            type="password"
+            placeholder="Password"
+            className="border-2 border-blue-600 p-2 w-full rounded-md"
+          />
+        </div>
+
+        <div>
+          <button className="w-full bg-blue-600  mt-6 py-3 rounded-md text-white text-2xl">
+            Login
+          </button>
+        </div>
+      </form>
+      
+        <p className="text-red-600 mt-3">Don't have an account? <Link to="/register">Register</Link></p>
+       
+    
+    </div>
+
+    </div>
+  );
 }
 
-export default Login
+export default Login;
--- src/Auth/Register.js
+++ src/Auth/Register.js
@@ -1,9 +1,68 @@
-import React from 'react'
+import { Link } from "react-router-dom";
+import React from "react";
 
 function Register() {
   return (
-    <div>Register</div>
-  )
+    <div className="mx-5">
+      <div className="flex flex-col justify-center mx-10 shadow-lg mt-10 px-5 rounded-md mb-10 bg-gray-50">
+        <h1 className=" font-bold text-3xl text-center pt-3">Create Account</h1>
+
+        <form>
+          <div className="mt-6">
+            <input
+              type="text"
+              placeholder="Full Name"
+              className="border-2 border-blue-600 p-2 w-full rounded-md"
+            />
+          </div>
+
+          <div className="mt-6">
+            <input
+              type="email"
+              placeholder="Email"
+              className="border-2 border-blue-600 p-2 w-full rounded-md"
+            />
+          </div>
+
+          <div className="mt-6">
+            <input
+              type="number"
+              placeholder="Mobile Number"
+              className="border-2 border-blue-600 p-2 w-full rounded-md"
+            />
+          </div>
+
+          <div className="mt-6">
+            <input
+              type="password"
+              placeholder="Enter Password"
+              className="border-2 border-blue-600 p-2 w-full rounded-md"
+            />
+          </div>
+
+          <div className="mt-6">
+            <input
+              type="password"
+              placeholder="Confirm Password"
+              className="border-2 border-blue-600 p-2 w-full rounded-md"
+            />
+          </div>
+
+          <div>
+            <button className="w-full bg-blue-600  mt-6 py-3 rounded-md text-white text-2xl">
+              Register
+            </button>
+          </div>
+
+          
+            <p className="text-blue-600 mt-3 pb-10">
+              have an account? <Link to="/login">Login</Link>
+            </p>
+          
+        </form>
+      </div>
+    </div>
+  );
 }
 
-export default Register
+export default Register;
