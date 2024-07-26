c91f8f0feeccf4d371f5018e0d0e40115f0ec2f6
MOHAMED ABDIHAKIN MUHUMED <48615938+Nucmaan@users.noreply.github.com>
2024-07-27 07:27:05 +0800

    updating px-3
    updating px-3

--- src/Renter/Bills.js
+++ src/Renter/Bills.js
@@ -10,10 +10,10 @@ function Bills() {
       <SideBar />
     </div>
 
-    <div className="p-5 bg-white">
+    <div className="px-2 bg-white">
     <div className="pt-2">
       <h1 className="text-red-700 text-2xl font-bold mb-2">
-        Due Amount: <span className="text-black">RM: 200</span>
+        Due Amount: <span className="text-black">RM: 00.00</span>
       </h1>
       <p className="text-lg text-black font-bold mb-2">
         Contract Signed: <span className="">13/09/2020</span>
@@ -26,19 +26,21 @@ function Bills() {
       <table className="w-full border-collapse">
         <thead className="bg-black text-white">
           <tr>
+          <th className="p-2 border">Ref No</th>
+          <th className="p-2 border">Month</th>
             <th className="p-2 border">Rent</th>
             <th className="p-2 border">Utilities</th>
-            <th className="p-2 border">Dues</th>
             <th className="p-2 border">Total</th>
             <th className="p-2 border">Action</th>
           </tr>
         </thead>
         <tbody>
           <tr className="text-black text-center bg-gray-100 odd:bg-gray-200 ">
+          <td className="p-2 border">123</td>
+          <td className="p-2 border">January</td>
             <td className="p-2 border">450</td>
             <td className="p-2 border">100</td>
-            <td className="p-2 border">200</td>
-            <td className="p-2 border">5750</td>
+            <td className="p-2 border">550</td>
             <td className="p-2 border">
               <button className="text-white px-5 py-2 bg-blue-500 rounded-md hover:bg-blue-600 transition-colors duration-300">
                 Pay Now
