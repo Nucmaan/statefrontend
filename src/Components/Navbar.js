1af4a583faf539887694c740450b189a190b9c63
MOHAMED ABDIHAKIN MUHUMED <48615938+Nucmaan@users.noreply.github.com>
2024-07-28 03:16:07 +0800

    adding navbar to profile image icon
    adding navbar to profile image icon

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -2,13 +2,20 @@ import { Link } from "react-router-dom";
 import React, { useState } from "react";
 import { AiOutlineClose } from "react-icons/ai";
 import { GiHamburgerMenu } from "react-icons/gi";
+import { FaUserCircle } from "react-icons/fa";
 import logo from "../Images/MyHomeLogo.png";
 
 function Navbar() {
   const [navbar, setNavbar] = useState(false);
+  const [isLogin, setIsLogin] = useState(true);
+  const [dropdown, setDropdown] = useState(false);
 
-  const handlenav = () => {
+  const handleNav = () => {
     setNavbar(!navbar);
+  };
+
+  const handleDropdown = () => {
+    setDropdown(!dropdown);
   };
 
   return (
@@ -41,17 +48,38 @@ function Navbar() {
         </div>
 
         <div>
-          <ul className="hidden md:flex space-x-4">
-            <li className="p-4 cursor-pointer uppercase flex items-center hover:text-gray-400">
-              <Link to="login">Login</Link>
-            </li>
-            <li className="p-4 cursor-pointer uppercase flex items-center hover:text-gray-400">
-              <Link to="register">Register</Link>
-            </li>
-          </ul>
+          {isLogin ? (
+            <div className="relative">
+              <div className="p-4 cursor-pointer uppercase flex items-center hover:text-gray-400" onClick={handleDropdown}>
+                <FaUserCircle size={30} />
+              </div>
+              {dropdown && (
+                <ul className="absolute right-0 mt-2 w-48 bg-white text-black shadow-lg rounded-lg overflow-hidden z-20">
+                  <li className="p-2 hover:bg-gray-200">
+                    <Link to="/Dashboard">Dashboard</Link>
+                  </li>
+                  <li className="p-2 hover:bg-gray-200">
+                    <Link to="/Profile">Profile</Link>
+                  </li>
+                  <li className="p-2 hover:bg-gray-200">
+                    <Link to="/" onClick={() => setIsLogin(false)}>Logout</Link>
+                  </li>
+                </ul>
+              )}
+            </div>
+          ) : (
+            <ul className="hidden md:flex space-x-4">
+              <li className="p-4 cursor-pointer uppercase flex items-center hover:text-gray-400">
+                <Link to="login">Login</Link>
+              </li>
+              <li className="p-4 cursor-pointer uppercase flex items-center hover:text-gray-400">
+                <Link to="register">Register</Link>
+              </li>
+            </ul>
+          )}
         </div>
 
-        <div className="md:hidden flex items-center" onClick={handlenav}>
+        <div className="md:hidden flex items-center" onClick={handleNav}>
           {navbar ? (
             <AiOutlineClose size={30} className="text-white" />
           ) : (
@@ -79,14 +107,16 @@ function Navbar() {
         <li className="w-full flex justify-center border-b border-gray-600 p-4 cursor-pointer uppercase hover:bg-gray-200">
           <Link to="Contact">Contact Us</Link>
         </li>
-        <div className="w-full flex flex-col items-center">
-          <button className="mb-4 bg-blue-600 text-white p-2 font-bold uppercase w-3/4">
-            <Link to="login">Login</Link>
-          </button>
-          <button className="mb-4 bg-blue-600 text-white p-2 font-bold uppercase w-3/4">
-            <Link to="register">Register</Link>
-          </button>
-        </div>
+        {!isLogin && (
+          <div className="w-full flex flex-col items-center">
+            <button className="mb-4 bg-blue-600 text-white p-2 font-bold uppercase w-3/4">
+              <Link to="login">Login</Link>
+            </button>
+            <button className="mb-4 bg-blue-600 text-white p-2 font-bold uppercase w-3/4">
+              <Link to="register">Register</Link>
+            </button>
+          </div>
+        )}
       </ul>
     </div>
   );
