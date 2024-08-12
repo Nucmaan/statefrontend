67a012ba1d111f8407decc905e19178e1c748dc3
MOHAMED ABDIHAKIN MUHUMED <48615938+Nucmaan@users.noreply.github.com>
2024-08-13 02:42:55 +0800

    addproduction
    addproduction

--- src/Redux/Store.js
+++ src/Redux/Store.js
@@ -23,6 +23,7 @@ export const store = configureStore({
   reducer: persistedReducer,
   middleware: (getDefaultMiddleware) => getDefaultMiddleware({
     serializableCheck: false,
+    
   }),
 });
 
--- src/index.js
+++ src/index.js
@@ -1,20 +1,35 @@
+require('dotenv').config();
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { persistor, store } from './Redux/Store'; // Correctly import the named export
-import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
-import "slick-carousel/slick/slick.css"; // Import slick carousel CSS
-import "slick-carousel/slick/slick-theme.css"; // Import slick carousel theme CSS
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+import { disableReactDevTools } from '@fvilers/disable-react-devtools';
+
+if (process.env.NODE_ENV === 'production') disableReactDevTools();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-    <PersistGate loading={null} persistor={persistor}> 
+
+if (process.env.NODE_ENV !== 'production') {
+  const { Provider } = require('react-redux');
+  const { PersistGate } = require('redux-persist/integration/react');
+  const { store, persistor } = require('./Redux/Store');
+
+  root.render(
+    <React.StrictMode>
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <App />
+        </PersistGate>
+      </Provider>
+    </React.StrictMode>
+  );
+} else {
+  root.render(
+    <React.StrictMode>
       <App />
-      </PersistGate>
-    </Provider>
-  </React.StrictMode>
-);
+    </React.StrictMode>
+  );
+}
