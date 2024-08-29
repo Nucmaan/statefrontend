1029d541b14134d505bddbeced5c0cbf0fb84a99
MOHAMED ABDIHAKIN MUHUMED <48615938+Nucmaan@users.noreply.github.com>
2024-08-30 03:26:43 +0800

    changing script
    changing script

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL = 'http://localhost:5000';
+const baseURL = 'https://backendm2u.myhome2u.online';
 
 //http://localhost:5000
 //https://backendm2u.myhome2u.online
